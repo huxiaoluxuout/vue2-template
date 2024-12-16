bdf149669ffdf26efba2f835e7fd23b5f4b8b7b3
huxiaolux <huxiaolux@outlook.com>
2024-12-16 17:45:10 +0800

    1.添加config.js 2.yalx.lock

---
+++ config.js
@@ -0,0 +1,13 @@
+System.config({
+    "paths": {
+        "@/api/*": "./api/*",
+        "@/components/*": "./components/*",
+        "@/store/*": "./store/*",
+        "@/uni_modules/*": "./uni_modules/*",
+        "@/utils/*": "./utils/*",
+        "@/lib/*": "./lib/*",
+        "@/static/*": "./static/*",
+        "@/common/*": "./common/*",
+        "@/*": "./*",
+    }
+});
